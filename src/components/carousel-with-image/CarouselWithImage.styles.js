a44742c0a42589d5f718d299b1db4615d801a9ed
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-01-04 16:35:16 +0200

    added feature block section (#17)

---
+++ src/components/carousel-with-image/CarouselWithImage.styles.js
@@ -0,0 +1,24 @@
+export const styles = {
+  feature: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    px: '16px',
+    mb: '16px'
+  },
+  image: {
+    width: '100%',
+    height: 'auto'
+  },
+  text: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    minHeight: '144px',
+    backgroundColor: 'primary.900',
+    borderRadius: '6px',
+    p: '16px'
+  }
+}
